aab81ea19ee02175d915f2fbf8ed1e1a5a33df22
galobon123 <galobon12310@gmail.com>
2025-09-13 15:37:12 -0300

    Correcion de irregularidades

--- WebAPI-Practica01/obj/Debug/net8.0/WebAPI-Practica01.AssemblyInfo.cs
+++ WebAPI-Practica01/obj/Debug/net8.0/WebAPI-Practica01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI-Practica01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6644846cb3a621cd7e357b258396cf9cb26baca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e6831b26acd85e350abd177d74b121d28ce051b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI-Practica01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI-Practica01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
